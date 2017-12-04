adca283fc516062770a395696f9a763504c97f08
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-12-03 23:50:22 -0500

    bands exercise

---
+++ bands/bands/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bands
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string venue = "C's Blues";
+            int bands = 4;
+            Console.WriteLine(venue + " will have " + bands + " bands performing tonight!");
+        }
+    }
+}
